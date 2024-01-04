b2b45e74cc6e2f6f825f39677b7e2d72ab306268
jhonatancamasca <jccamascah@gmail.com>
2024-01-04 15:48:40 -0500

    agregando el paquete

---
+++ textproccessing/tests/integration/test_integration_testing.py
@@ -0,0 +1,16 @@
+import pytest
+from processing import remove_links,remove_hastags
+
+@pytest.mark.parametrize(
+    "input_text, expected_output",
+    [
+        ("Check out this #awesome link: http://example.com", "Check out this  link: "),
+        ("No hashtags or links here!", "No hashtags or links here!"),
+        (123, 123),  
+    ],
+)
+def test_remove_hashtags_and_links(input_text, expected_output):
+    processed_text = remove_hastags(remove_links(input_text))
+    assert processed_text == expected_output
+    
+    
